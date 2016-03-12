42cac28d13488704d6ebbdac65114c95687793b0
Semion Kiselev <semion.kiselev@gmail.com>
2016-03-12 16:53:33 +0300

    test commit

--- src/app.js
+++ src/app.js
@@ -3,4 +3,6 @@ import fixtures from './fixtures'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
+console.log('test');
+
 ReactDOM.render(<ArticleList articles = {fixtures} />, document.getElementById('container'))
