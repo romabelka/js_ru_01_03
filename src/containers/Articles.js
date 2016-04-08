6dac0809898331277f3fc6e1c162854817deaf59
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-04-08 16:51:21 +0300

    add articles functionality

--- src/containers/Articles.js
+++ src/containers/Articles.js
@@ -1,4 +1,6 @@
 import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { deleteArticle } from '../actions/articles'
 
 class Articles extends Component {
     static propTypes = {
@@ -6,12 +8,30 @@ class Articles extends Component {
     };
 
     render() {
-        return (
-            <div>
-                <h1>Implement Me</h1>
-            </div>
+        const { articles } = this.props
+        const items = articles.map((article) =>
+            <li key = {article.id}>
+                {article.title}
+                |
+                <a href="#" onClick = {this.handleDelete(article.id)}> delete this article</a>
+            </li>
         )
+        return (
+            <ul>
+                {items}
+            </ul>
+        )
+    }
+
+    handleDelete = id => ev => {
+        ev.preventDefault()
+        this.props.deleteArticle(id)
     }
 }
 
-export default Articles
+export default connect((state) => {
+    const { articles } = state
+    return { articles }
+}, {
+    deleteArticle
+})(Articles)
