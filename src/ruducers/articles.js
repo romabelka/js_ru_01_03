d9986b3083ea686dda416ca55c10de1851072575
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-04-08 20:24:46 +0300

    fix reducer

--- src/ruducers/articles.js
+++ src/ruducers/articles.js
@@ -5,7 +5,8 @@ export default (articles = defaultState, action) => {
     const { type, data, response } = action
 
     switch (type) {
-        case DELETE_ARTICLE: return articles.filter(article => article.id != data.id)
+        case DELETE_ARTICLE: return Object.assign({}, articles,
+            {entities: articles.entities.filter(article => article.id != data.id)})
         case LOAD_ALL_ARTICLES + START: return {...articles, loading: true}
         case LOAD_ALL_ARTICLES + SUCCESS: return {...articles, loading: false, loaded: true, entities: response}
     }
